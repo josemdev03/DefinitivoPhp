380c8b27dda010b196570717c0c64a663e4fff5e
Jose Mendoza <jose-fernando07@outlook.com>
2024-10-10 01:04:21 -0500

    jeje

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    @include('layouts.head')
+
+    <body>
+
+        @include('layouts.header')
+
+        @include('layouts.sidebarMenu')
+
+        <main id="main" class="main">
+
+            @yield('content')
+            
+        </main>
+
+        @include('layouts.footer')
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        @include('layouts.scripts')
+        
+    </body>
+</html>
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,11 @@
+<script src="{{ asset('theme/vendor/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('theme/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('theme/vendor/chart.js/chart.umd.js') }}"></script>
+<script src="{{ asset('theme/vendor/echarts/echarts.min.js') }}"></script>
+<script src="{{ asset('theme/vendor/quill/quill.js') }}"></script>
+<script src="{{ asset('theme/vendor/simple-datatables/simple-datatables.js') }}"></script>
+<script src="{{ asset('theme/vendor/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('theme/vendor/php-email-form/validate.js') }}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{ asset('theme/js/main.js') }}"></script>
